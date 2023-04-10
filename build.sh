2c6e9070aed941458d89682ba0bc144b50ae975e
Helmut Waldschmidt <st171805@stud.uni-stuttgart.de>
2023-04-10 09:14:42 +0200

    add release/debug build flags

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+debug_flag=''
+while getopts 'dr' flag; do
+    case "${flag}" in
+        d) debug_flag='Debug' ;;
+        r) debug_flag='Release' ;;
+        *) print_usage
+            exit 1 ;;
+    esac
+done
+
+echo "$debug_flag Build"
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_BUILD_TYPE="$debug_flag" ..
 make
